47da4b785430d8c4f65bf3556f17a41e83a02c2c
corvofeng <corvofeng@gmail.com>
2018-03-19 21:26:02 +0800

    少量修改文件
    Signed-off-by: corvofeng <corvofeng@gmail.com>

---
+++ analyzer/common/packet.h
@@ -0,0 +1,25 @@
+/*
+ *=======================================================================
+ *    Filename:packet.h
+ *
+ *     Version: 1.0
+ *  Created on: March 19, 2018
+ *
+ *      Author: corvo
+ *=======================================================================
+ */
+
+#ifndef PACKET_H_WL06DAMQ
+#define PACKET_H_WL06DAMQ
+
+/**
+ * **吐槽一下**: 今天的工作白做了, 当我发现了下面的网页
+ *    http://www.tcpdump.org/sniffex.c, 我发现自己傻得像个两百斤的胖子
+ *    而后又发现了 "/usr/include/netinet/in.h",
+ *    我开始明白, 自己是个两百五十斤的胖子.
+ */
+
+
+
+
+#endif /* end of include guard: PACKET_H_WL06DAMQ */
--- controler/main.py
+++ controler/main.py
@@ -21,12 +21,13 @@ define('port', default=9999, help='run on the given port', type=int)
 define('debug', default=False, help="debug or relase", type=bool)
 define('autoreload', default=True, help="when modify auto reolad", type=bool)
 
+
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/v1/test", TeshHandler),
         ]
-        settings = dict (
+        settings = dict(
             debug=options.debug,
             autoreload=options.autoreload
         )
@@ -42,19 +43,9 @@ def main():
     q_listen()
 
     SLOG.info("App is listening: %d" % options.port)
-
-    # 定时任务的设定
-    # tornado.ioloop.PeriodicCallback(main_task.task, main_task.TASK_INTERVAL).start()
-
-    # main_task.task()
-    #log_calculate.log_cal(options.debug)
-    # if is_same_ip(options.task_run_ip):
-    #    SLOG.info('The task should run')
-    #    log_calculate.log_cal_run(options.debug)
     tornado.ioloop.IOLoop.current().start()
 
 
 if __name__ == "__main__":
-    #tornado.options.parse_command_line()
     tornado.options.parse_config_file('./server.conf')
     main()
