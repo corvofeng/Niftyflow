9cf328a548a4cc5ed8e7978dbc9ad26b56cd47d8
corvofeng <corvofeng@gmail.com>
2018-03-24 22:08:44 +0800

    添加了测试用例, 进行测试开发时, 可以查看该代码
    Signed-off-by: corvofeng <corvofeng@gmail.com>

---
+++ analyzer/test/ExampleTest.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+/**
+ * @brief 直接将此文件复制即可, 可直接运行`./test/ExampleTest`
+ *
+ * @return 返回非0说明测试失败;
+ */
+int main() {
+    printf("Hello Everflow\n");
+    return 0;
+}
+
