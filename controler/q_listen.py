ef33613e84bf6feadc076f112a42ebc27994311a
corvofeng <corvofeng@gmail.com>
2018-03-27 22:04:57 +0800

    控制器与分析器交互成功
    Signed-off-by: corvofeng <corvofeng@gmail.com>

--- controler/q_listen.py
+++ controler/q_listen.py
@@ -18,6 +18,25 @@ def q_listen():
     c = tornadoredis.Client()
     c.connect()
     while True:
-        d = yield tornado.gen.Task(c.blpop, 'q')
-        print(d)
+        msg = yield tornado.gen.Task(c.blpop, 'ever_queue')
+        parse_request(msg)
+
+
+def parse_request(msg):
+    """解析客户端来的消息
+    """
+    print(msg)
+
+
+def deal_alert(msg):
+    """处理预警
+    """
+    pass
+
+
+def deal_request(msg):
+    """处理请求, 主要是分析器初始化时的请求
+    """
+    pass
+
 
